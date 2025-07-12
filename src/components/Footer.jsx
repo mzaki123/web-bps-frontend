f12fbac1c35b7f15d93eb306d528a8b36584cb7f
user123 <imadudinzaki111715@gmail.com>
2025-07-12 19:36:04 +0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+import React from "react";
+export default function Footer() {
+  return (
+    <footer className="text-center py-8">
+      <p className="text-gray-500 text-sm">
+        Created by Falana Rofako Hakam (222112038@stis.ac.id)        
+      </p>
+      <p className="text-gray-500 text-sm">
+        Developed by Muhammad Imaddudin Zaki (222313244@stis.ac.id)
+      </p>
+    </footer>
+  );
+}
